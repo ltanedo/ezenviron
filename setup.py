2a4200da18b6152dd977b18deea8aa3fee5675cc
ltanedo <lloydtan@buffalo.edu>
2025-09-04 14:52:27 -0400

    Prepare 0.3.0 release

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
-    name="ezenviron",
+setup(    name="ezenviron",
     version="0.3.0",
     author="ltanedo",
     author_email="lloydtan@buffalo.edu",
@@ -13,15 +12,12 @@ setup(
     license="MIT",
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
-    entry_points={
-        "console_scripts": [
-            "ezenviron=ezenviron.__init__:main",
-        ],
-    },
+    entry_points={"console_scripts": ["ezenviron=ezenviron.__init__:main"]},
     install_requires=[],
     keywords=["windows", "environment", "variables", "cli", "utilities"],
     classifiers=["Development Status :: 3 - Alpha", "Intended Audience :: Developers", "License :: OSI Approved :: MIT License", "Operating System :: Microsoft :: Windows", "Programming Language :: Python :: 3", "Programming Language :: Python :: 3.7", "Programming Language :: Python :: 3.8", "Programming Language :: Python :: 3.9", "Programming Language :: Python :: 3.10", "Programming Language :: Python :: 3.11"],
-    project_urls={"Bug Reports": "https://github.com/ltanedo/ezenviron/issues","Source": "https://github.com/ltanedo/ezenviron","Documentation": "https://github.com/ltanedo/ezenviron#readme"},
+    project_urls={"Bug Reports": "https://github.com/ltanedo/ezenviron/issues", "Source": "https://github.com/ltanedo/ezenviron", "Documentation": "https://github.com/ltanedo/ezenviron#readme"},
     long_description=long_description,
     packages=find_packages(),
 )
+
