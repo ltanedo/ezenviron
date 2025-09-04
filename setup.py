36c2fa6aa9dc4cd5c5c02303337cd165c3928304
Lloyd Tanedo <ltanedo@nysif.com>
2025-09-04 14:07:31 -0400

    Prepare 0.3.0 release

--- setup.py
+++ setup.py
@@ -3,23 +3,41 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(    name="ezenviron",
+setup(
+    name="ezenviron",
     version="0.3.0",
     author="ltanedo",
     author_email="lloydtan@buffalo.edu",
     description="A python module enabling CRUD on Windows user env vars for safe API development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/ltanedo/ezenviron",
     license="MIT",
-    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "ezenviron=ezenviron.__init__:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
     python_requires=">=3.7",
-    entry_points="",
-    console_scripts="",
-    - "ezenviron=ezenviron.__init__="main",
     install_requires=[],
     keywords=["windows", "environment", "variables", "cli", "utilities"],
-    classifiers=["Development Status :: 3 - Alpha", "Intended Audience :: Developers", "License :: OSI Approved :: MIT License", "Operating System :: Microsoft :: Windows", "Programming Language :: Python :: 3", "Programming Language :: Python :: 3.7", "Programming Language :: Python :: 3.8", "Programming Language :: Python :: 3.9", "Programming Language :: Python :: 3.10", "Programming Language :: Python :: 3.11"],
-    project_urls={"Bug Reports": "https://github.com/ltanedo/ezenviron/issues", "Source": "https://github.com/ltanedo/ezenviron", "Documentation": "https://github.com/ltanedo/ezenviron#readme"},
-    long_description=long_description,
-    packages=find_packages(),
+    project_urls={
+        "Bug Reports": "https://github.com/ltanedo/ezenviron/issues",
+        "Source": "https://github.com/ltanedo/ezenviron",
+        "Documentation": "https://github.com/ltanedo/ezenviron#readme",
+    },
 )
 
